959d49f18d96ae50015c5ada5fde561f88339b63
1DAW-mgutval <mgutval@g.educaand.es>
2026-03-10 13:00:27 +0100

    Empezado diapositivas excepciones

---
+++ Tema 6 (Excepciones y Ficheros)/Boletin/src/main/java/Ejercicios/Ej101/Main.java
@@ -0,0 +1,17 @@
+package Ejercicios.Ej101;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+//    public static Integer leerEntero() {
+//        Scanner sc = new Scanner(System.in);
+//
+//        try {
+//
+//        }
+//    }
+}
