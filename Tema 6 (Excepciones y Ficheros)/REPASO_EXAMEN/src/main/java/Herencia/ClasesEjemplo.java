3e6e27d9a991722e74c915959f80b8b4b5cec9dd
mariogv07 <mario.gutierrez.valencia07@gmail.com>
2026-03-19 22:30:18 +0100

    Repaso examen Terminado AHORA SI

--- Tema 6 (Excepciones y Ficheros)/REPASO_EXAMEN/src/main/java/Herencia/ClasesEjemplo.java
+++ Tema 6 (Excepciones y Ficheros)/REPASO_EXAMEN/src/main/java/Herencia/ClasesEjemplo.java
@@ -4,4 +4,10 @@ public class ClasesEjemplo {
 //    Lleva la cuenta de los participantes, puede ser publico porque es static, se inicializa en el constructor.
     static int ultimoNumParticipante = 0;
 
+
+//    Si una interfaz extiende de otra, no se pone implements, se pone extends.
+
+
+//    Para ordenar de forma inversa, primero se crea un Comparator normal y corriente. Después esta sintaxis: Comparator comp2 = comp1.reversed()
+//    Después: Arrays.sort(lista,comp2);
 }
